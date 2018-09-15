a60ea2439fba961a32e5fd4f8db1b225a6be804a
Estela Parrado <estela.parrado@gmail.com>
2018-09-16 00:07:25 +0200

    restylish spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -5,7 +5,7 @@ class Spinner extends Component {
     render() {
         return (
             <span className="page__main--loader">
-                <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg">
+                <svg width="100" height="100" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg">
                     <defs>
                         <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
                             <stop stopColor="#FFDF00" stopOpacity="0" offset="0%" />
